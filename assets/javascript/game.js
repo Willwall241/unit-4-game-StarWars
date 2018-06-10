4f86f646855d61774367cb50c4e54fca3bb90d83
WJW <will.j.waller@gmail.com>
2018-06-09 20:32:23 -0400

    update

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,44 +1,59 @@
 var gameOn = true;
 var storedPower = 0;
 var defenderEmpty = true;
+var defName = "";
+var fighter = "";
 var currentHp = 0;
+var currentAttack = 0;
 var enemyHp = 0;
 var enemyKill = 0;
 
 
 var availableChar = [
-
+  //Mace Windu Obj
   {
     name: "Mace Windu",
     hp: 160,
     attack: 10,
     counter: 12,
+    //Method to switch character selected to fighter
     switchFighter: function () {
       $("#charOne").removeClass("select").addClass("fighter");
       $("#charTwo").removeClass("select").addClass("enemy");
       $("#charThree").removeClass("select").addClass("enemy");
       $("#charFour").removeClass("select").addClass("enemy");
     },
+
+    //Method to move character
     moveChar: function () {
       if (($("#charOne").hasClass("select") && defenderEmpty == true) || ($("#charOne").hasClass("fighter") && defenderEmpty == true)) {
         $("#charOne").appendTo("#activeChar");
         $("#charTwo").appendTo("#enemies");
         $("#charThree").appendTo("#enemies");
         $("#charFour").appendTo("#enemies");
+        fighter = "Mace";
+        currentHp = Mace.hp;
+        currentAttack = Mace.attack;
       }
-      else if ($("#charOne").hasClass("enemy") && ($("#defender").is(':empty'))) {
+      else if ($("#charOne").hasClass("enemy") && defenderEmpty == true) {
+        $("#charOne").addClass("target");
         $("#charOne").appendTo("#defender");
+        $("#fight-text").text("");
         defenderEmpty = false;
+        defName = "Mace";
+        enemyHp = Mace.hp;
       }
     },
   },
 
+  //Darth Vader Obj
   {
     name: "Darth Vader",
     hp: 170,
     attack: 12,
     counter: 20,
 
+    //Method to switch character selected to fighter
     switchFighter: function () {
       $("#charTwo").removeClass("select").addClass("fighter");
       $("#charOne").removeClass("select").addClass("enemy");
@@ -46,6 +61,7 @@ var availableChar = [
       $("#charFour").removeClass("select").addClass("enemy");
     },
 
+    //Method to move character
     moveChar: function () {
 
       if (($("#charTwo").hasClass("select") && defenderEmpty == true) || ($("#charTwo").hasClass("fighter") && defenderEmpty == true)) {
@@ -53,21 +69,30 @@ var availableChar = [
         $("#charOne").appendTo("#enemies");
         $("#charThree").appendTo("#enemies");
         $("#charFour").appendTo("#enemies");
+        fighter = "Vader";
+        currentHp = Vader.hp;
+        currentAttack = Vader.attack;
       }
       else if ($("#charTwo").hasClass("enemy") && defenderEmpty == true) {
+        $("#charTwo").addClass("target");
         $("#charTwo").appendTo("#defender");
+        $("#fight-text").text("");
         defenderEmpty = false;
+        defName = "Vader";
+        enemyHp = Vader.hp;
       }
     }
 
   },
 
+  //Yoda Object
   {
     name: "Yoda",
     hp: 200,
     attack: 15,
     counter: 25,
 
+    //Method to switch character selected to fighter
     switchFighter: function () {
       $("#charThree").removeClass("select").addClass("fighter");
       $("#charOne").removeClass("select").addClass("enemy");
@@ -75,6 +100,7 @@ var availableChar = [
       $("#charFour").removeClass("select").addClass("enemy");
     },
 
+    //Method to move character
     moveChar: function () {
 
       if (($("#charThree").hasClass("select") && defenderEmpty == true) || ($("#charThree").hasClass("fighter") && defenderEmpty == true)) {
@@ -82,21 +108,31 @@ var availableChar = [
         $("#charOne").appendTo("#enemies");
         $("#charTwo").appendTo("#enemies");
         $("#charFour").appendTo("#enemies");
+        fighter = "Yoda";
+        currentHp = Yoda.hp;
+        currentAttack = Yoda.attack;
       }
-      else if ($("#charThree").hasClass("enemy") && ($("#defender").is(':empty'))) {
+      else if ($("#charThree").hasClass("enemy") && defenderEmpty == true) {
+        $("#charThree").addClass("target");
         $("#charThree").appendTo("#defender");
+        $("#fight-text").text("");
         defenderEmpty = false;
+        defName = "Yoda";
+        enemyHp = Yoda.hp;
       }
     }
 
   },
 
+
+  //Count Dooku Obj
   {
     name: "Count Dooku",
     hp: 150,
     attack: 9,
     counter: 15,
 
+    //Method to switch character selected to fighter
     switchFighter: function () {
       $("#charFour").removeClass("select").addClass("fighter");
       $("#charOne").removeClass("select").addClass("enemy");
@@ -104,6 +140,7 @@ var availableChar = [
       $("#charTwo").removeClass("select").addClass("enemy");
     },
 
+    //Method to move character
     moveChar: function () {
 
       if (($("#charFour").hasClass("select") && defenderEmpty == true) || ($("#charFour").hasClass("fighter") && defenderEmpty == true)) {
@@ -111,10 +148,17 @@ var availableChar = [
         $("#charOne").appendTo("#enemies");
         $("#charTwo").appendTo("#enemies");
         $("#charThree").appendTo("#enemies");
+        fighter = "Dooku";
+        currentHp = Dooku.hp;
+        currentAttack = Dooku.attack;
       }
-      else if ($("#charFour").hasClass("enemy") && ($("#defender").is(':empty'))) {
+      else if ($("#charFour").hasClass("enemy") && defenderEmpty == true) {
+        $("#charFour").addClass("target");
         $("#charFour").appendTo("#defender");
+        $("#fight-text").text("");
         defenderEmpty = false;
+        defName = "Dooku";
+        enemyHp = Dooku.hp;
       }
     }
   },
@@ -130,10 +174,11 @@ var Dooku = availableChar[3];
 
 
 function startGame() {
-  $(".mace-hP").text(Mace.hp);
-  $(".vader-hP").text(Vader.hp);
-  $(".yoda-hP").text(Yoda.hp);
-  $(".dooku-hP").text(Dooku.hp);
+  $(".mace-hp").text(Mace.hp);
+  $(".vader-hp").text(Vader.hp);
+  $(".yoda-hp").text(Yoda.hp);
+  $(".dooku-hp").text(Dooku.hp);
+  $("#reset").hide();
 }
 
 $("#charOne").on("click", function () {
@@ -152,7 +197,7 @@ $("#charTwo").on("click", function () {
 
   if (gameOn && $("#charTwo").hasClass("select")) {
 
-    
+
     Vader.switchFighter();
 
   }
@@ -164,7 +209,7 @@ $("#charThree").on("click", function () {
 
   if (gameOn && $("#charThree").hasClass("select")) {
 
-    
+
     Yoda.switchFighter();
 
   }
@@ -177,12 +222,173 @@ $("#charFour").on("click", function () {
 
   if (gameOn && $("#charFour").hasClass("select")) {
 
-    
+
     Dooku.switchFighter();
 
   }
 
   Dooku.moveChar();
+
+});
+
+
+$("#attack").on("click", function () {
+
+  if (fighter == "Mace" && defName == "Vader" && gameOn) {
+
+    currentHp = currentHp - Vader.counter;
+    $(".mace-hp").text(currentHp);
+    currentAttack = currentAttack + Mace.attack;
+    enemyHp = enemyHp - currentAttack;
+    $(".vader-hp").text(enemyHp);
+    checkWin();
+
+  }
+  else if (fighter == "Mace" && defName == "Yoda" && gameOn) {
+
+    currentHp = currentHp - Yoda.counter;
+    $(".mace-hp").text(currentHp);
+    currentAttack = currentAttack + Mace.attack;
+    enemyHp = enemyHp - currentAttack;
+    $(".yoda-hp").text(enemyHp);
+    checkWin();
+
+  }
+  else if (fighter == "Mace" && defName == "Dooku" && gameOn) {
+
+    currentHp = currentHp - Dooku.counter;
+    $(".mace-hp").text(currentHp);
+    currentAttack = currentAttack + Mace.attack;
+    enemyHp = enemyHp - currentAttack;
+    $(".dooku-hp").text(enemyHp);
+    checkWin();
+
+  }
+
+  if (fighter == "Vader" && defName == "Mace" && gameOn) {
+
+    currentHp = currentHp - Mace.counter;
+    $(".vader-hp").text(currentHp);
+    currentAttack = currentAttack + Mace.attack;
+    enemyHp = enemyHp - currentAttack;
+    $(".mace-hp").text(enemyHp);
+    checkWin();
+
+  }
+  else if (fighter == "Vader" && defName == "Yoda" && gameOn) {
+
+    currentHp = currentHp - Yoda.counter;
+    $(".vader-hp").text(currentHp);
+    currentAttack = currentAttack + Mace.attack;
+    enemyHp = enemyHp - currentAttack;
+    $(".yoda-hp").text(enemyHp);
+    checkWin();
+
+  }
+  else if (fighter == "Vader" && defName == "Dooku" && gameOn) {
+
+    currentHp = currentHp - Dooku.counter;
+    $(".vader-hp").text(currentHp);
+    currentAttack = currentAttack + Mace.attack;
+    enemyHp = enemyHp - currentAttack;
+    $(".dooku-hp").text(enemyHp);
+    checkWin();
+
+  }
+
+  if (fighter == "Yoda" && defName == "Mace" && gameOn) {
+
+    currentHp = currentHp - Mace.counter;
+    $(".yoda-hp").text(currentHp);
+    currentAttack = currentAttack + Mace.attack;
+    enemyHp = enemyHp - currentAttack;
+    $(".mace-hp").text(enemyHp);
+    checkWin();
+
+  }
+  else if (fighter == "Yoda" && defName == "Vader" && gameOn) {
+
+    currentHp = currentHp - Vader.counter;
+    $(".yoda-hp").text(currentHp);
+    currentAttack = currentAttack + Mace.attack;
+    enemyHp = enemyHp - currentAttack;
+    $(".vader-hp").text(enemyHp);
+    checkWin();
+
+  }
+  else if (fighter == "Yoda" && defName == "Dooku" && gameOn) {
+
+    currentHp = currentHp - Dooku.counter;
+    $(".yoda-hp").text(currentHp);
+    currentAttack = currentAttack + Mace.attack;
+    enemyHp = enemyHp - currentAttack;
+    $(".dooku-hp").text(enemyHp);
+    checkWin();
+
+  }
+
+  if (fighter == "Dooku" && defName == "Mace" && gameOn) {
+
+    currentHp = currentHp - Mace.counter;
+    $(".dooku-hp").text(currentHp);
+    currentAttack = currentAttack + Mace.attack;
+    enemyHp = enemyHp - currentAttack;
+    $(".mace-hp").text(enemyHp);
+    checkWin();
+
+  }
+  else if (fighter == "Dooku" && defName == "Yoda" && gameOn) {
+
+    currentHp = currentHp - Yoda.counter;
+    $(".dooku-hp").text(currentHp);
+    currentAttack = currentAttack + Mace.attack;
+    enemyHp = enemyHp - currentAttack;
+    $(".yoda-hp").text(enemyHp);
+    checkWin();
+
+  }
+  else if (fighter == "Dooku" && defName == "Vader" && gameOn) {
+
+    currentHp = currentHp - Vader.counter;
+    $(".dooku-hp").text(currentHp);
+    currentAttack = currentAttack + Mace.attack;
+    enemyHp = enemyHp - currentAttack;
+    $(".vader-hp").text(enemyHp);
+    checkWin();
+
+  }
+
+});
+
+
+function checkWin() {
+
+  if (enemyHp <= 0 && enemyKill < 2 ) {
+
+    $(".target").remove();
+    defenderEmpty = true;
+    $("#fight-text").text("You defeated " + defName + " you can choose to fight another enemy.")
+    enemyKill++;
+
+  }
+  else if ((currentHp <=0 && enemyKill !== 3)) {
+    $("#fight-text").text("You Lose, restart the game and try again!");
+    gameOn = false;
+    $("#reset").show();
+  }
+  else if (currentHp >= 0 && enemyHp <=0 && enemyKill === 2) {
+    $(".target").remove();
+    $("#fight-text").text("You Won, good job restart game to try again");
+    gameOn = false;
+    $("#reset").show();
+
+  }
+
+};
+
+$("#reset").on("click", function(){
+
+  location.reload();
 
 });
 
